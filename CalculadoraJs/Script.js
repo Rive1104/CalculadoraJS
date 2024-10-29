0ac83ea0f59d868940472081870457e26cee43e7
Rive1104 <22690380@tecvalles.mx>
2024-10-28 20:02:04 -0600

    Add files via upload

---
+++ CalculadoraJs/Script.js
@@ -0,0 +1,58 @@
+
+
+
+function suma() {
+
+
+
+    valor1 = document.getElementsByName("num1")[0].value;
+
+    valor2 = document.getElementsByName("num2")[0].value;
+
+    suma = parseInt(valor1) + parseInt(valor2);
+
+    alert("El resultado es " + suma);
+
+};
+
+function multi() {
+
+
+
+    valor1 = document.getElementsByName("num1")[0].value;
+
+    valor2 = document.getElementsByName("num2")[0].value;
+
+    prod = parseInt(valor1) * parseInt(valor2);
+
+    alert("El resultado es " + prod);
+
+};
+
+function resta() {
+
+
+
+    valor1 = document.getElementsByName("num1")[0].value;
+
+    valor2 = document.getElementsByName("num2")[0].value;
+
+    resta = parseInt(valor1) - parseInt(valor2);
+
+    alert("El resultado es " + resta);
+
+};
+
+function divi() {
+
+
+
+    valor1 = document.getElementsByName("num1")[0].value;
+
+    valor2 = document.getElementsByName("num2")[0].value;
+
+    divi = parseInt(valor1) / parseInt(valor2);
+
+    alert("El resultado es " + divi);
+
+};
